66e1ca7c57bd7d342c5fa2917f27b02bb5467b97
Victoria-Butrym <victoria-butrym@tut.by>
2019-10-09 18:25:49 +0300

    feat: add solution to Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let bracketsArray = str.split("");
+  let brConfig = bracketsConfig;
+
+  for(let i = 0; i < bracketsArray.length; i++) {
+      for(let j = 0; j < brConfig.length; j++) {
+        if(bracketsArray[i] === brConfig[j][0] && bracketsArray[i + 1] === brConfig[j][1]) {
+            bracketsArray.splice(i, 2);
+
+            if( bracketsArray.length === 0) {
+                return true;
+            }
+
+            i = -1;
+        }
+      }
+  }
+  return false
 }
